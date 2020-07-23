a0ea7bd303b53f1c29782df397a4d6ddfeab7fcb
Your NameRyan <Rymaster95@gmail.com>
2020-07-23 14:51:07 -0400

    updates

--- drawing-ui/src/app/lobby/lobby.component.ts
+++ drawing-ui/src/app/lobby/lobby.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../chat.service';
 
 @Component({
   selector: 'app-lobby',
@@ -7,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LobbyComponent implements OnInit {
 
-  public players: any = [{name: "test"}];
+  public players: any = [];
   public newWord
 
   displayedColumns: string[] = ['name'];
 
-  constructor() { }
+  constructor(private socketService: ChatService) { }
 
   ngOnInit(): void {
+    this.socketService
+    .getUsers()
+    .subscribe((users: string[]) => {
+      this.players = users
+      console.log(this.players);
+
+    })
   }
 
   startGame(){
--- server/index.js
+++ server/index.js
@@ -9,32 +9,49 @@ let io = socketIO(server);
 
 const port = 3005;
 let rooms = [];
+let users = [];
 //room{name: name, users: ['Ryan']}
 
 io.on("connection", function(socket) {
-    console.log("a user connected");
     socket.emit("rooms", rooms)
     
     socket.on("join_room", ({name, room}) => {
-        if (rooms.filter(x => x == room).length != 1){
-            rooms.push(room)
-        }
+        joinRoom(name, socket.id , room)
+       
+        console.log(rooms);
+        console.log(name +' joined ' + room);
 
-        console.log(name +' joined room ' + room);
       socket.join(room);
-      io.to(room).emit(room.users)
+      console.log(rooms.filter(x => x.name== room.name)[0].users);
+      io.to(room).emit('users', rooms.filter(x => x.name== room.name)[0].users)
       io.emit("rooms", rooms)
     });
   
-    socket.on("message", ({ room, message }) => {
-        console.log("message recieved in " + room + message);
-      io.to(room).emit("message", message);
-    });
+    // socket.on("message", ({ room, message }) => {
+    //     console.log("message recieved in " + room + message);
+    //   io.to(room).emit("message", message);
+    // });
   
-    socket.on("typing", ({ room }) => {
-      socket.to(room).emit("typing", "Someone is typing");
+    // socket.on("typing", ({ room }) => {
+    //   socket.to(room).emit("typing", "Someone is typing");
+    // });
+
+    socket.on('disconnect', () => {
+      
+      console.log(socket.id);
+      console.log('user left');
+
+      
     });
 });
+
+function joinRoom(name, socket, room) {
+  if (rooms.filter(x => x.name == room.name).length != 1){
+    rooms.push({room: room, users: [{Name: name, socket: socket}]})
+  } else {
+    rooms.filter(x => x.name== room.name)[0].users.push({Name: name, socket: socket})
+  } 
+}
 
 server.listen(port, () => {
     console.log(`started on port: ${port}`);
